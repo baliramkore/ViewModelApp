f9e24269ae138d81412f7603c9596ed74db01502
baliram <baliramkore@yahoo.com>
2024-08-31 12:42:22 +0530

    created simple view which updates count on button click

--- app/src/main/java/com/rbk/viewmodelapp/MainActivity.kt
+++ app/src/main/java/com/rbk/viewmodelapp/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.rbk.viewmodelapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var count:Int=0
+    lateinit var textCounter:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        textCounter=findViewById(R.id.textView)
+
+    }
+
+    fun increment(view: View) {
+        count++
+        setText()
+
+    }
+
+    private fun setText() {
+        textCounter.text=count.toString()
     }
 }
